dc04c5db6599472b6e86c4b4613e03e822f5d1f2
Randy <randyisbusy@gmail.com>
2020-04-17 17:36:18 -0700

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,23 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let hidden = document.getElementById("modal");
+hidden.setAttribute("class", "hidden"); 
+document.addEventListener("click", function response(e){ 
+  e.target.matches(".like") || e.target.matches(".like-glyph") ? fillHeart(e) : null
+}) 
 
-
+function fillHeart(){ 
+  if (event.target.matches(".like-glyph")){ 
+    event.target.setAttribute("class", "activated-heart");   
+    let heart = event.target; 
+    heart.innerHTML = FULL_HEART;
+  } else if (event.target.matches(".like")) { 
+    event.target.children[0].setAttribute("class", "activated-heart"); 
+    let heart = event.target.children[0] 
+    heart.innerHTML = FULL_HEART;
+  }
+}
 
 
 //------------------------------------------------------------------------------
